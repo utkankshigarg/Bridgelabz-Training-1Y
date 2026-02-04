f7a642380e24c2802558e10ba9fde463d87d94bc
Utkankshi Garg <gargutkankshi2006@gmail.com>
2026-02-04 09:28:25 +0530

    finding age of Harry

---
+++ basic-programs/AgeofHarry.java
@@ -0,0 +1,8 @@
+public class AgeofHarry{
+  public static void main(String[] args){
+  int birthyear=2000;
+  int currentyear=2026;
+  int age=currentyear-birthyear;
+  System.out.println("Harry age in 2026 is"+age);
+  }
+  }
