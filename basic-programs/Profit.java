b177b98a1f4d8a955c3d2f9b622ccf1c53eca07c
Utkankshi Garg <gargutkankshi2006@gmail.com>
2026-02-04 22:36:49 +0530

    profit

---
+++ basic-programs/Profit.java
@@ -0,0 +1,10 @@
+public class Profit{
+      public static void main(String[] args){
+      int costprice=129;
+	  int sellingprice=191;
+      double Profit=sellingprice-costprice;
+      double Profitpercentage=Profit/costprice*100;
+	System.out.println("The costprice is INR"+costprice+"and sellingprice is INR"+sellingprice);
+	System.out.println("The Profit is INR"+Profit+"and the Profitpercentage is "+Profitpercentage);
+	}
+	}
