1972010be05013f14e3df6064c6d84383f648051
Utkankshi Garg <gargutkankshi2006@gmail.com>
2026-02-04 22:34:59 +0530

    distribute

---
+++ basic-programs/Distribution.java
@@ -0,0 +1,9 @@
+public class Distribution{
+        public static void main(String[] args){
+        int pens=14;
+        int students=3;
+        int result=pens/students;
+        int remaining=pens%students;
+   System.out.println("The pen per student is"+result+"and the remaining pen not distributed is"+remaining);
+   }
+   }
