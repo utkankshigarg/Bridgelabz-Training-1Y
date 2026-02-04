8a0ea583be1f60d0fc3dc9a3a8b0ff85f6bb65b1
Utkankshi Garg <gargutkankshi2006@gmail.com>
2026-02-04 22:37:40 +0530

    square side

---
+++ basic-programs/SquareSide.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class SquareSide
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new
+		Scanner(System.in);
+	System.out.print("Enter the perimeter of the square:");
+	double perimeter=sc.nextDouble();
+	double side = perimeter / 4;
+	System.out.println("The lenght of the side is" +side +" whose perimeter is " +perimeter);
+	}
+}
