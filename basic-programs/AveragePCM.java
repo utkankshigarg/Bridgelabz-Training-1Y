aab7244617a3c6928f9ef40903da353c36ac514f
Utkankshi Garg <gargutkankshi2006@gmail.com>
2026-02-04 22:15:17 +0530

    finding average

---
+++ basic-programs/AveragePCM.java
@@ -0,0 +1,9 @@
+public class AveragePCM{
+   public static void main(String[] args){
+   int Mathsmarks=94;
+   int Physicsmarks=95;
+   int Chemistrymarks=96;
+   double Average=(Mathsmarks+ Physicsmarks+Chemistrymarks)/3.0;
+   System.out.print("Sam Average marks in PCM is "+Average);
+   }
+   }
